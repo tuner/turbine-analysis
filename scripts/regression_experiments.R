7bae040b020d94e64f93211cba2c6bc59ccd5977
Kari Lavikka <kari.lavikka@bdb.fi>
2018-10-15 18:36:37 +0300

    Experiments with glmnet

--- scripts/regression_experiments.R
+++ scripts/regression_experiments.R
@@ -12,23 +12,39 @@ library(readr)
 combined_data <- read_delim("../derived_data/combined_data.csv", 
                             "\t", escape_double = FALSE, trim_ws = TRUE)
 
+# Subset the data by columns
 turbines <- combined_data[, 1:18]
 census <- combined_data[, 19:38]
 
-#model <- lm(combined_data$`Construction of wind turbines would have a positive impact on Helsinki's image.` ~ combined_data[, 19:38])
+
 #model <- lm(turbines$`Construction of wind turbines would have a positive impact on Helsinki's image.`
-#            ~ ., data = census[, 14:20])
+#            ~ ., data = census[, c(1, 2, 9, 10)])
 
 #summary(model)
 
 
+explain_variable <- function(variable) {
+  model <- glmnet(as.matrix(census), variable)
+  
+  # Use cross-validation to find good lambda
+  model.cv <- cv.glmnet(as.matrix(census), variable)
+  lambda <- cvmodel$lambda.min
+  
+  # Plot, leave intercept out
+  barplot(t(as.matrix(coef(model, s = lambda)))[, -1], las = 2)
+}
+
+explain_variable(turbines$`Local wind power should be produced in Helsinki`)
+
+
+# Simple linear regression
 model_and_plot <- function(x, y) {
-  model <- lm(x ~ y)
-  plot(y, x)
+  # Read more at: http://r-statistics.co/Linear-Regression.html
+  model <- lm(y ~ x)
+  plot(x, y)
   abline(model, col = "green")
   
-  text(x = y,
-       y = x,
+  text(x, y,
        labels = combined_data$`District name`,
        col = "#404040",
        cex = 0.5,
@@ -38,12 +54,12 @@ model_and_plot <- function(x, y) {
   print(paste("Correlation:", cor(y, x)))
 }
 
-model_and_plot(turbines$`Construction of wind turbines would have a positive impact on Helsinki's image.`, census$VIHR)
-model_and_plot(turbines$`Construction of wind turbines would have a positive impact on Helsinki's image.`, census$KOK)
-model_and_plot(turbines$`Construction of wind turbines would have a positive impact on Helsinki's image.`, census$VASL)
+model_and_plot(census$VIHR, turbines$`Construction of wind turbines would have a positive impact on Helsinki's image.`)
+model_and_plot(census$KOK, turbines$`Construction of wind turbines would have a positive impact on Helsinki's image.`)
+model_and_plot(census$VASL, turbines$`Construction of wind turbines would have a positive impact on Helsinki's image.`)
 
-model_and_plot(turbines$`I would like to buy local wind power in Helsinki, even if cheaper electricity would be available from elsewhere`, census$`Households without cars`)
-model_and_plot(turbines$`What do you feel if wind turbines would be built at the outer territorial waters (8-10 km from the coastline) of Helsinki?`, census$KOK)
+model_and_plot(census$`Households without cars`, turbines$`I would like to buy local wind power in Helsinki, even if cheaper electricity would be available from elsewhere`)
+model_and_plot(census$KOK, turbines$`What do you feel if wind turbines would be built at the outer territorial waters (8-10 km from the coastline) of Helsinki?`)
 
 
 # Pretty plot of correlations
