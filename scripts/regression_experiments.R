5e8938d95fc2348740cce13884a0cff67597eb8c
Kari Lavikka <kari.lavikka@bdb.fi>
2018-10-17 11:03:45 +0300

    Updates pitch with Alko and some other stuff

--- scripts/regression_experiments.R
+++ scripts/regression_experiments.R
@@ -26,7 +26,7 @@ explain_variable <- function(variable) {
 explain_variable(data$turbine$`Local wind power should be produced in Helsinki`)
 explain_variable(data$turbine$`I would like to buy local wind power in Helsinki, even if cheaper electricity would be available from elsewhere`)
 explain_variable(data$turbine$`What do you feel if wind turbines would be built at the outer territorial waters (8-10 km from the coastline) of Helsinki?`)
-
+explain_variable(data$turbine$`How often do you spend your free time on the shores of Helsinki?`)
 
 
 model_and_plot(data$census$VIHR, data$turbine$`Construction of wind turbines would have a positive impact on Helsinki's image.`)
