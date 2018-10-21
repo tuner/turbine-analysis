d6345edefa57b623e0574d8bf08d32780a452185
dvy1989 <dobrodeevvy@gmail.com>
2018-10-22 00:32:43 +0300

    Now factors names are printed dynamically. However, it is needed to decide how to print plotly charts dynmaically

--- scripts/visualisation_experiments.R
+++ scripts/visualisation_experiments.R
@@ -4,62 +4,74 @@ library(sp)
 library(shiny)
 
 # Read districts shapes
+# Data are read for 2015 as an actual poll was done that year
 helsinki_districts <- spTransform(
-  readOGR(file.path("..","raw_data","piirialuejako-1995-2016.gpkg"), layer="perus_2016"),
+  readOGR(file.path("..","raw_data","piirialuejako-1995-2016.gpkg"), layer="perus_2015"),
   CRS("+proj=longlat")
 )
 
-names(helsinki_districts)[names(helsinki_districts)=="PERUS"] <- "District.id"
+# Prepare districts data set for merge
+names(helsinki_districts)[names(helsinki_districts)=="PERUS"] <- "District id"
 
-data <- read.csv(file.path("..", "derived_data", "combined_data.csv"), sep="\t", fileEncoding = "utf8", encoding = "utf8")
-
-districts <- merge(helsinki_districts, data, by="District.id")
+# Read "census" data
+census_data <- read.csv(file.path("..", "derived_data", "combined_data.csv"), sep="\t", fileEncoding = "utf8", encoding = "utf8", check.names = FALSE)
 
 rb_options <- list()
 
-for (i in 8:23){
-  rb_options[gsub("\\."," ", names(districts)[i])] <- i
+for (i in 2:17){
+  rb_options[names(census_data)[i]] <- i + 6
 }
+
+# Merge census data and districts shapes
+combined_data <- merge(helsinki_districts, census_data, by="District id")
+
+factors <- 24:length(names(combined_data@data))
+plots_id <- lapply(factors, function(x){ paste0(names(combined_data@data)[x], "plot") })
+tabs <- lapply(factors, function(x){
+  tabPanel(names(combined_data@data)[x], plotOutput(plots_id[x - min(factors)]))
+})
 
 ui <- fluidPage(
   titlePanel("Helsinki map"),
-  sidebarLayout(position = "right",
+  sidebarLayout(
                 sidebarPanel("Questions",
-                radioButtons("question", label="", choices = rb_options)
+                  radioButtons("question", label="", choices = rb_options)
                 ),
                 mainPanel("Helsinki map",
                           leafletOutput("helsinki_map"),
-                          textOutput("selected_region")
+                          uiOutput("Factors")
                 )
   )
-
 )
 
 server <- function(input, output, session){
   output$helsinki_map <- renderLeaflet({
     column_id<-strtoi(input$question)
-    leaflet(districts) %>%
+    leaflet(combined_data) %>%
         addTiles() %>%
         fitBounds(24.78516,60.09772, 25.27679, 60.31403) %>%
         addPolygons(
           weight=1,
-          fillColor=~colorNumeric("PiYG", districts@data[,column_id])(districts@data[,column_id]),
+          fillColor=~colorNumeric("PiYG", -2:2)(combined_data@data[,column_id]),
           fillOpacity = 0.5,
-          layerId = ~District.id
+          layerId = ~`District id`
         ) %>%
       addLegend(
         position="bottomright", 
-        pal=colorNumeric("PiYG", districts@data[,column_id]),
-        values=~districts@data[,column_id],
-        title=gsub("\\.", " ", names(districts@data)[column_id])
+        pal=colorNumeric("PiYG", -2:2),
+        values=~combined_data@data[,column_id],
+        title=names(combined_data@data)[column_id]
       )
+  }) 
+  
+  output$Factors <- renderUI({
+    do.call(tabsetPanel, tabs)
   })
+  
+  lapply(plots_id, function(x) { print(output[x])})
 
   observeEvent(input$helsinki_map_shape_click, {
     click <- input$helsinki_map_shape_click
-    output$selected_region <- renderText(
-		as.character.factor(districts[districts$District.id==click$id,]$Nimi)
-    )
   })
 }
 
